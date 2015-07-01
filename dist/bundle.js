8c703319bd72f5bda30938bed5d95dfe4352ddc8
Willem Odendaal <willem@newmedialabs.co.za>
2015-07-01 15:41:36 +0200

    Basic gulp working with watchify and node.

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * Created by willem on 7/1/15.
+ */
+"use strict";
+
+var a = function a() {
+  return 10;
+};
+
+alert(a(77));
+var ab = 10;
+
+},{}]},{},[1]);
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by willem on 7/1/15.
+ */
+var a = () => {return 10;}
+
+
+alert(a(77));
+var ab = 10;
