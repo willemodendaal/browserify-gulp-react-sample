86a8054e14236b31382ca9a0f85c64e73b7d3177
Willem Odendaal <willem@newmedialabs.co.za>
2015-07-01 16:57:57 +0200

    Added react, and rendered first component.

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,16 @@
  * Created by willem on 7/1/15.
  */
 var math = require('./lib/math');
+var React = require('react');
 
-console.log('xyz');
+var MainView = React.createClass({
+    render() {
+        "use strict";
+
+        return (
+            <div>MAINDIV</div>
+        );
+    }
+});
+
+React.render(<MainView/>, document.querySelector('.content'));
